bc42de67b6a93f438d7b4e65981e7204237d0576
littlett98 <37678140+littlett98@users.noreply.github.com>
2020-12-10 14:25:59 -0500

    Added methods for view products without logging in. Added method for viewing products and adding stuff to cart. Changed login and register a bit to improve their functionality.

--- CoffeeShop/src/frontend/App.java
+++ CoffeeShop/src/frontend/App.java
@@ -11,21 +11,32 @@ public class App {
 	private static Scanner input = new Scanner(System.in);
 	private static Customer c;
 	private static Database coffeeShop = new Database();
+	private static Cart cart = new Cart();
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+		boolean loggedIn = false;
 		System.out.println("Welcome to Trevors Coffee Shop!");
 		System.out.println("Would you like to log in or create a new account?");
 		System.out.println("Enter 1 to login");
 		System.out.println("Enter 2 to register");
+		System.out.println("Enter 3 to view our products");
 		int selection = input.nextInt();
 		if (selection == 1) {
 			login();
+			loggedIn = true;
 		}
 		else if (selection == 2) {
 			register();
+			loggedIn = true;
+		}
+		else if (selection == 3) {
+			viewProducts();
 		}
 		else {
 			System.out.println("Invalid input");
+		}
+		if (loggedIn) {
+			viewProductsLoggedIn();
 		}
 	}
 	
@@ -52,7 +63,7 @@ public class App {
 		if (selection == 1) {
 			System.out.println("What is the email of the client who referred you?");
 			String refer_id = coffeeShop.getCustIDByEmail(input.next());
-			c = new Customer(username, password, address, phone, email, refer_id);
+			c = new Customer(username, address, phone, email, refer_id);
 			try {
 				coffeeShop.addCustomer(c);
 			} catch (SQLException e) {
@@ -60,7 +71,7 @@ public class App {
 			}
 		}
 		else if (selection == 2) {
-			c = new Customer(username, password, address, phone, email, null);
+			c = new Customer(username, address, phone, email, null);
 			try {
 				coffeeShop.addCustomer(c);
 			} catch (SQLException e) {
@@ -76,10 +87,51 @@ public class App {
 		System.out.println("You are on the login page");
 		DatabaseSecurity userCreation = new DatabaseSecurity();
 		try {
-			System.out.println(userCreation.login());
+			c = userCreation.login();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-
+	
+	public static void viewProducts() throws SQLException {
+		System.out.println("These are the current products we offer:");
+		Product[] products = coffeeShop.getAllProducts();
+		for (int i = 0; i < products.length; i++) {
+			System.out.println((i+1) + ". " + products[i].getName() + " - $" + products[i].getRetailPrice());
+		}
+	}
+	
+	public static void viewProductsLoggedIn() throws SQLException {
+		boolean shopping = true;
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Welcome " + c.getUsername() + "!");
+		while(shopping) {
+			viewProducts();
+			System.out.println("Which products would you like to add to your cart?");
+			String selection = reader.nextLine();
+			String[] allSelections = selection.split(" ");
+			int[] allSelectionsNum = new int[allSelections.length];
+			for (int i = 0; i < allSelections.length; i++) {
+				allSelectionsNum[i] = Integer.parseInt(allSelections[i]);
+			}
+			addToCart(allSelectionsNum);
+			System.out.println("What would you like to do? 1. Continue shopping 2. Check Out");
+			int next = reader.nextInt();
+			if (next == 1) {
+				continue;
+			}
+			else if (next == 2) {
+				shopping = false;
+			}
+		}
+		reader.close();
+		//viewCart();
+	}
+	
+	public static void addToCart(int[] allSelections) throws SQLException {
+		Product[] allProducts = coffeeShop.getAllProducts();
+		for (int i = 0; i < allSelections.length; i++) {
+			cart.addProduct(allProducts[allSelections[i]]);
+		}
+	}
 }
