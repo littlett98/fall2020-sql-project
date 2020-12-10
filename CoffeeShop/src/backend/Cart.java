5e0fda1c3899bdf3284f774e66de678155f7c86d
littlett98 <37678140+littlett98@users.noreply.github.com>
2020-12-10 14:26:45 -0500

    Added consturctor and appropriate methods to cart

--- CoffeeShop/src/backend/Cart.java
+++ CoffeeShop/src/backend/Cart.java
@@ -1,5 +1,41 @@
 package backend;
 
-public class Cart {
+import tables.Product;
 
+public class Cart {
+	
+	private Product[] products;
+	
+	public Cart() {
+		products = new Product[10];
+	}
+	
+	public void addProduct(Product p) {
+		if (productArrayFull()) {
+			increaseArraySize();
+		}
+		for (int i = 0; i < products.length; i++) {
+			if (products[i] == null) {
+				products[i] = p;
+				break;
+			}
+		}
+	}
+	
+	public boolean productArrayFull() {
+		for (int i = 0; i < products.length; i++) {
+			if (products[i] == null) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public void increaseArraySize() {
+		Product[] temp = new Product[products.length + 10];
+		for (int i = 0; i < products.length; i++) {
+			temp[i] = products[i];
+		}
+		products = temp;
+	}
 }
